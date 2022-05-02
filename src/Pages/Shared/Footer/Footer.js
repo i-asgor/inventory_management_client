338f64c29ff8af82ae05fbe0df34db52573f40b3
Asgor Ali <i.joni40@gmail.com>
2022-05-02 15:22:07 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const date = today.getFullYear();
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className='bg-dark'>
+            <div className="container">
+                <div className="row justify-content-between">
+                    <div className="col-sm-12 col-md-6 text-start">
+                        <p className='text-white'> <small> &copy; {date} copyright Electronics Inventory Management</small></p>
+                    </div>
+                    <div className="col-sm-12 col-md-6 text-end">
+                        <p className='text-white'>
+                            <a href="https://web.facebook.com" className='text-white text-decoration-none'>Facebook</a>
+                            <a href="https://www.linkedin.com" className='text-white text-decoration-none mx-2'>Linkdin</a>
+                            <a href="https://www.youtube.com" className='text-white text-decoration-none'>YouTube</a>
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+        </footer>
     );
 };
 
