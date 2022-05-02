a786b549f7688c243af005f4c0106d103610396f
Asgor Ali <i.joni40@gmail.com>
2022-05-02 15:17:46 +0600

    React Router with navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+           <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+            <Container>
+                
+            <Navbar.Brand as={Link} to="/" className='d-flex align-items-center'>
+                <img src={logo} alt="logo" style={{width:'50px'}} />
+                <span className='ms-2'>Electronics Inventory</span>
+                </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>                
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </>
     );
 };
 
